0f6f6e93ca360c8d656f584a4613f715f742c5e6
Mousa Ayoubi <mousaayoubi@gmail.com>
2019-01-20 14:55:13 +0200

    Update app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+// Declare a variable myAge and set its value
+let myAge = 40;
+
+// Declare a variable for earlyYears and set its value
+let earlyYears = 2;
+
+earlyYears = earlyYears * 10.5;
+
+// Declare a variable laterYears and subtract two years from it
+let laterYears = myAge - 2;
+
+// Multipy laterYears by four
+laterYears = laterYears * 4;
+
+console.log(earlyYears, laterYears);
+
+// Declare a variable myAgeInDogYears and store the value of the sume of earlyYears and LaterYears in it
+let myAgeInDogYears = earlyYears + laterYears;
+
+// Declare a variable myName and store the value of my name in it in lower case.
+let myName = "Mousa".toLowerCase();
+
+// Log the statement of my age in dog years to the console
+console.log(
+  `My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`
+);
